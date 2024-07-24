727f2e9ecb89296519c711f0acf89b0477077862
Janani M <141489026+MJ-Janani@users.noreply.github.com>
2024-07-24 12:28:22 +0530

    Update Double Pointer.c

--- Double Pointer.c
+++ Double Pointer.c
@@ -6,4 +6,7 @@ int main(){
     printf("a= %d\n",a);//10
     printf("*p= %d\n",*p);//*(2000)= 10
     printf("**p=%d\n",**pp);//**(3000)= *(2000)= 10
+    //example
+    **pp=*p+25;
+    printf("a value is changed to %d\n",a);//35
 }
